b10917be006c3d373f5610b8084b2a28042715fe
carlsirce <carlsirce@126.com>
2014-08-31 09:42:24 +0800

    Create FileUploadField.js

---
+++ FileUploadField.js
@@ -0,0 +1,33 @@
+
+if (!Paims.component)
+    Ext.namespace('Paims.component');
+
+Paims.component.FileUploadField = function (params) {
+    Paims.component.FileUploadField.superclass.constructor.call(this, params);
+}
+Ext.extend(Paims.component.FileUploadField, Ext.form.FileUploadField, {
+    validator: function () {
+        var x = this.getValue();
+        if (!x)
+            return;
+        if (this.fileTypes.length == 0)
+            return true;
+
+        var patn = '/'
+        var docType = '';
+        for (var i = 0; i < this.fileTypes.length; i++) {
+            patn += '\.' + this.fileTypes[i] + '$|';
+            docType += this.fileTypes[i] + '，';
+        }
+        patn = patn.substring(0, patn.length - 1);
+        patn += '/i';
+        patn = eval(patn);
+
+        docType = docType.substring(0, docType.length - 1);
+        if (!patn.test(x)) {
+            this.invalidText = '只能上传' + docType + '文档。';
+            return false;
+        }
+        return true;
+    }
+});
